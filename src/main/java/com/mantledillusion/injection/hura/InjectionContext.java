1c89396b478f7f37386a3c77704491c48a1ad14d
tobias.weber <tobias.weber@ecotel.de>
2018-03-21 09:11:51 +0100

    Fixed property predefining in the root injector and property inheriting between injection sequences not working

--- src/main/java/com/mantledillusion/injection/hura/InjectionContext.java
+++ src/main/java/com/mantledillusion/injection/hura/InjectionContext.java
@@ -1,32 +1,70 @@
 package com.mantledillusion.injection.hura;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
-import com.mantledillusion.injection.hura.annotation.Construct;
+import com.mantledillusion.injection.hura.Injector.SelfSustaningProcessor;
+import com.mantledillusion.injection.hura.exception.ProcessorException;
 
 class InjectionContext {
 	
 	static final String INJECTION_CONTEXT_SINGLETON_ID = "_injectionContext";
+	
+	static class GlobalInjectionContext extends InjectionContext {
 
-	private final Map<String, Object> singletonBeans;
-
-	@Construct 
-	InjectionContext() {
-		this(new HashMap<>());
+		private final Map<String, List<SelfSustaningProcessor>> destroyables = new HashMap<>();
+		
+		GlobalInjectionContext() {
+		}
+		
+		<T> void addGlobalSingleton(String singletonId, T instance, List<Processor<? super T>> destroyers) {
+			List<SelfSustaningProcessor> destroyables = new ArrayList<>();
+			for (Processor<? super T> destroyer : destroyers) {
+				destroyables.add(() -> destroyer.process(instance, null));
+			}
+			this.destroyables.put(singletonId, destroyables);
+			super.addSingleton(singletonId, instance);
+		}
+		
+		void destroy() {
+			Iterator<Entry<String, List<SelfSustaningProcessor>>> iter = this.destroyables.entrySet().iterator();
+			while (iter.hasNext()) {
+				Entry<String, List<SelfSustaningProcessor>> entry = iter.next();
+				try {
+					for (SelfSustaningProcessor destroyable: entry.getValue()) {
+						destroyable.process();
+					}
+					super.removeSingleton(entry.getKey());
+				} catch (Exception e) {
+					throw new ProcessorException("Unable to destroy global singleton '"+entry.getKey()+"': "+e.getMessage(), e);
+				}
+			}
+		}
 	}
 
-	InjectionContext(InjectionContext baseContext) {
+	private final Map<String, Object> singletonBeans;
+	
+	private InjectionContext() {
+		this(null, null);
+	}
+	
+	InjectionContext(ResolvingContext resolvingContext) {
+		this(null, resolvingContext);
+	}
+
+	InjectionContext(InjectionContext baseContext, ResolvingContext resolvingContext) {
 		this.singletonBeans = new HashMap<>();
 		if (baseContext != null) {
 			this.singletonBeans.putAll(baseContext.singletonBeans);
 		}
 		this.singletonBeans.put(InjectionContext.INJECTION_CONTEXT_SINGLETON_ID, this);
-	}
-	
-	private InjectionContext(Map<String, Object> singletonBeans) {
-		this.singletonBeans = singletonBeans;
-		this.singletonBeans.put(InjectionContext.INJECTION_CONTEXT_SINGLETON_ID, this);
+		if (resolvingContext != null) {
+			this.singletonBeans.put(ResolvingContext.RESOLVING_CONTEXT_SINGLETON_ID, resolvingContext);
+		}
 	}
 
 	boolean hasSingleton(String singletonId) {
